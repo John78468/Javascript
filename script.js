976b027b921d5984009b4bec54141fda5466e47e
John78468 <73955607+John78468@users.noreply.github.com>
2021-08-20 01:48:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log("Bonjour monde");
+
+let valeur = prompt("ENvoi ton nom?");
+console.log 
+
+
+let statement = `Dans mon groupe on est ${numStudent} moussaillons`;
